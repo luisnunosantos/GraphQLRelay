cdb45bf33642bf1bb118d5c3a1698c804ca0306d
Luís Santos <nuno.santos@gmail.com>
2018-04-30 14:24:15 +0100

    DB Connection

--- GraphQLRelay/users/schema/schema.js
+++ GraphQLRelay/users/schema/schema.js
@@ -1,5 +1,4 @@
 const graphql = require('graphql');
-const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
@@ -8,11 +7,7 @@ const {
     GraphQLList,
     GraphQLNonNull
 } = graphql;
- 
-const users = [
-    { id: '1', firstName: 'Luis', age: '43' },
-    { id: '2', firstName: 'Nuno', age: '42 '}
-];
+const axios = require('axios');
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -30,7 +25,8 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args: { id: {type: GraphQLString }},
             resolve(parentValue, args){
-                return _.find(users, {id: args.id});
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                    .then(resp => resp.data);
             }
         }
     }
