736f3fa55565fde17d9182d2ebfe0dbe4a5c830d
Luís Santos <nuno.santos@gmail.com>
2018-04-30 12:28:36 +0100

    GraphQL Interface

---
+++ GraphQLRelay/users/schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = graphql;
+ 
+const users = [
+    { id: '1', firstName: 'Luis', age: '43' },
+    { id: '2', firstName: 'Nuno', age: '42 '}
+];
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName:{ type: GraphQLString },
+        age:{ type: GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: {type: GraphQLString }},
+            resolve(parentValue, args){
+                return _.find(users, {id: args.id});
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
